56ac4362a58914c5a7bf56752dcde517d40f2649
elboj <adaja.tobi1@gmail.com>
2023-03-10 14:09:22 -0600

    added tf files

---
+++ Week_1/Neblus-LB-SCUS/main.tf
@@ -0,0 +1,65 @@
+#***Provider block***#
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+#***Data Sources***#
+data "azurerm_resource_group" "resource-group" {
+  name = "NEU-RG-STG001"
+}
+
+data "azurerm_network_interface" "nic" {
+  name                = "NEU-NIC-P001"
+  resource_group_name = "NEU-RG-STG001"
+}
+
+#***Load Balancer***#
+resource "azurerm_lb" "load-balancer" {
+  name                = "NEU-LB-P001"
+  location            = data.azurerm_resource_group.resource-group.location
+  resource_group_name = data.azurerm_resource_group.resource-group.name
+  sku                 = "Standard"
+
+  #***EXPLICIT DEPENDENCY
+  depends_on = [
+    azurerm_public_ip.public-ip
+  ]
+
+  frontend_ip_configuration {
+    name                 = "ipconfig"
+    #***IMPLICIT DEPENDENCY
+    public_ip_address_id = azurerm_public_ip.public-ip.id
+  }
+}
+
+#***Creating a public Ip for the LB
+resource "azurerm_public_ip" "public-ip" {
+  name                = "NEU-PIP-P001"
+  location            = data.azurerm_resource_group.resource-group.location
+  resource_group_name = data.azurerm_resource_group.resource-group.name
+  sku                 = "Standard"
+  allocation_method   = "Static"
+}
+
+#***Backend Address Pool***#
+resource "azurerm_lb_backend_address_pool" "example" {
+  loadbalancer_id = azurerm_lb.load-balancer.id
+  name            = "NEU-BEP-P001"
+}
+
+#***Adding nic to backend pool
+resource "azurerm_network_interface_backend_address_pool_association" "example" {
+  network_interface_id    = data.azurerm_network_interface.nic.id
+  ip_configuration_name   = "NEU-IP-P001"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.example.id
+}
---
+++ Week_1/Neblus-VM-EastUS/main.tf
@@ -0,0 +1,72 @@
+#provider block
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "resource-group" {
+  name     = "NEU-RG-STG001"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "virtual-network" {
+  name                = "NEU-VN-P001"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.resource-group.location
+  resource_group_name = azurerm_resource_group.resource-group.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "NEU-WSB-P001"
+  resource_group_name  = azurerm_resource_group.resource-group.name
+  virtual_network_name = azurerm_virtual_network.virtual-network.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "NEU-NIC-P001"
+  location            = azurerm_resource_group.resource-group.location
+  resource_group_name = azurerm_resource_group.resource-group.name
+
+  ip_configuration {
+    name                          = "NEU-IP-P001"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "virtual-machine" {
+  name                = "NEU-WIN-P001"
+  resource_group_name = azurerm_resource_group.resource-group.name
+  location            = azurerm_resource_group.resource-group.location
+  size                = "Standard_F2"
+  admin_username      = "neblus"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
+
+
+
+
