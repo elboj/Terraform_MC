79bb160c6475e9923732a834bae4f199675fddd1
elboj <44838756+elboj@users.noreply.github.com>
2023-03-11 08:25:54 -0600

    Update main.tf

--- Week_1/Neblus-LB-SCUS/main.tf
+++ Week_1/Neblus-LB-SCUS/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_lb" "load-balancer" {
   ]
 
   frontend_ip_configuration {
-    name                 = "ipconfig"
+    name = "ipconfig"
     #***IMPLICIT DEPENDENCY
     public_ip_address_id = azurerm_public_ip.public-ip.id
   }
@@ -62,4 +62,23 @@ resource "azurerm_network_interface_backend_address_pool_association" "example" 
   network_interface_id    = data.azurerm_network_interface.nic.id
   ip_configuration_name   = "NEU-IP-P001"
   backend_address_pool_id = azurerm_lb_backend_address_pool.example.id
+}
+
+#***Adding health probe
+resource "azurerm_lb_probe" "health-probe" {
+  loadbalancer_id = azurerm_lb.load-balancer.id
+  name            = "http-running-probe"
+  port            = 80
+}
+
+#LB Rule
+resource "azurerm_lb_rule" "lb-rule" {
+  loadbalancer_id                = azurerm_lb.load-balancer.id
+  name                           = "LBRule"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.example.id]
+  probe_id                       = azurerm_lb_probe.health-probe.id
+  frontend_ip_configuration_name = "ipconfig"
 }
